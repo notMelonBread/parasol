6a6e1880ab8098bdd3472682104125b799192b30
IwatsukaYura <iwatsukayura@gmail.com>
2023-05-17 16:44:29 +0900

    画像の切り出し

--- predict_bottle.py
+++ predict_bottle.py
@@ -2,7 +2,7 @@ from keras.utils import load_img, img_to_array
 from keras.models import model_from_json
 import numpy as np
 from pathlib import Path
-
+import os
 
 # 画像が保存されているルートディレクトリのパス
 root_dir = Path(__file__).resolve().parent
@@ -17,24 +17,31 @@ model.load_weights(root_dir / "tea_predict.hdf5")
 
 
 # 画像を読み込む
-img_path = str(input("画像ファイル名を入力してください："))
-img = load_img(img_path, target_size=(250, 250, 3))
-x = img_to_array(img)
-x = np.expand_dims(x, axis=0)
+#ここをfor文で回してcut_photo内の写真を判定するような形でやる
+photo_dir = (root_dir / 'cut_photo') 
+files = os.listdir(photo_dir)
+for file in files:
+    img_path = (photo_dir / file)
+    img = load_img(img_path, target_size=(250, 250, 3))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
 
-# 予測
-features = model.predict(x)
+    # 予測
+    features = model.predict(x)
 
-# 予測結果によって処理を分ける
-if features[0, 0] == 1:
-    print("選ばれたのは、waterでした。")
+    # 予測結果によって処理を分ける
+    if features[0, 0] == 1:
+        print("選ばれたのは、waterでした。")
 
-elif features[0, 1] == 1:
-    print("選ばれたのは、teaでした。")
+    elif features[0, 1] == 1:
+        print("選ばれたのは、teaでした。")
 
-else:
-    for i in range(0, 10):
-        if features[0, i] == 1:
-            cat = categories[i]
-    message = "綾鷹を選んでください。（もしかして：あなたが選んでいるのは「" + cat + "」ではありませんか？）"
-    print(message)
+    else:
+        print('お目当ての商品はございません☆')
+
+    # else:
+    #     for i in range(0, 10):
+    #         if features[0, i] == 1:
+    #             cat = categories[i]
+    #     message = "綾鷹を選んでください。（もしかして：あなたが選んでいるのは「" + cat + "」ではありませんか？）"
+    #     print(message)
