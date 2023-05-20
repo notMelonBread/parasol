b41054688d023f35c3b44eb6e526e86894a49f8d
komekopan2 <yoo.shimizu888@gmail.com>
2023-05-20 15:34:53 +0900

    JSの削除、ルートディレクトリの変更がある

--- apps/parasol/app.py
+++ apps/parasol/app.py
@@ -1,24 +1,28 @@
-from flask import Flask,render_template,request
-import base64
+from flask import Flask, render_template, request, redirect, url_for
 import os
-app=Flask(__name__)
+import base64
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",data="ここに読み上げられるテキストがきます")
 
-@app.route("/upload_photo", methods=['POST'])
+@app.route("/upload_photo", methods=['GET','POST'])
 def upload_photo():
-    # OnClickVideoで画像を撮影する
-    photo_data = request.json['photo']
-    # 写真データをデコードして保存
-    save_photo(photo_data, 'static/photos', 'snapshot.png')
-    # 保存した写真を表示
-    return render_template("index.html")
+    if request.method == 'POST':
+        if "photo" in request.json:
+            photo_data = request.json['photo']
+            save_photo(photo_data, 'static/photos', 'snapshot.png')
+            return "写真が送信されました"
+        else:
+            return "写真が見つかりません"
+    else:
+        return render_template("index.html", data="お茶は上からn番目、左からm番目の位置にあります")
 
 def save_photo(photo_data, save_dir, file_name):
     # base64エンコードされた写真データをデコード
-    photo_bytes = base64.b64decode(photo_data.split(',')[1])
+    photo_bytes = photo_data.split(',')[1].encode('utf-8')
 
     # ファイルの保存先ディレクトリを作成
     os.makedirs(save_dir, exist_ok=True)
@@ -28,4 +32,7 @@ def save_photo(photo_data, save_dir, file_name):
 
     # 写真をファイルとして保存
     with open(file_path, 'wb') as f:
-        f.write(photo_bytes)
+        f.write(base64.b64decode(photo_bytes))
+
+if __name__ == "__main__":
+    app.run(debug=True)
